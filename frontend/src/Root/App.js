757dca484a3222c0b50e43b28189e3f29ba80a0c
krzlepianka <krzlepianka@gmail.com>
2020-08-09 19:07:13 +0200

    add login form

---
+++ frontend/src/Root/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../Theme/globalStyles';
+import {theme} from '../Theme/mainTheme';
+import Register from '../components/templates/Register';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <>
+            <Register />
+          </>
+        </ThemeProvider>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
