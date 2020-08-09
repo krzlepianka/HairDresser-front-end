e947b2de349ce74ef5b8e9f15e5165c329067996
Krzysztof Brak <krzlepianka@gmail.com>
2020-08-10 00:28:00 +0200

    third fixed commit

--- src/Root/App.js
+++ src/Root/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
           <>
             <Register />
             <Register />
-
+            <Register />
           </>
         </ThemeProvider>
       </React.Fragment>
